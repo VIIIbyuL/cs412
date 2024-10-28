4c0440cf4e99adac55405fe02b51d1e107fdd287
John Kim <johnjk@bu.edu>
2024-10-28 19:45:41 -0400

    assignment 8

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -38,6 +38,46 @@ class Profile(models.Model):
         '''
         return reverse('show_profile', kwargs={'pk': self.pk})
     
+    def get_friends(self):
+        '''
+        fetches friends for a profile
+        '''
+        friends1 = Friend.objects.filter(profile1=self).values_list('profile2', flat=True)
+        friends2 = Friend.objects.filter(profile2=self).values_list('profile1', flat=True)
+        friends_list = list(friends1) + list(friends2)
+        return list(Profile.objects.filter(id__in=friends_list))
+    
+    def get_news_feed(self):
+        '''
+        fetcehs the news feed for a profile
+        '''
+        friends = [friend.id for friend in self.get_friends()]
+        all_news = friends + [self.id]
+        feed = StatusMessage.objects.filter(profile__id__in=all_news).order_by('-timestamp')
+        return feed
+    
+    def add_friend(self, friend):
+        '''
+        adds a friend to a profile
+        '''
+        if self == friend:
+            return
+        
+        if Friend.objects.filter(profile1=self, profile2=friend).exists() or Friend.objects.filter(profile1=friend, profile2=self).exists():
+            return
+        
+        Friend.objects.create(profile1=self, profile2=friend)
+
+    def get_friend_suggestions(self):
+        '''
+        suggests profiles to be friends with
+        '''
+        friends_list = [friend.id for friend in self.get_friends()]
+
+        suggestions = Profile.objects.exclude(id__in=friends_list).exclude(id=self.id)
+        return suggestions
+
+    
 class StatusMessage(models.Model):
     '''
     StatusMessage model that shows status message
@@ -76,3 +116,19 @@ class Image(models.Model):
         returns a string representation of Image
         '''
         return f"{self.img_file}"
+    
+class Friend(models.Model):
+    '''
+    friend model that represents a friend relationship
+    '''
+
+    # data fields for a friend relationship
+    profile1 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile1')
+    profile2 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile2')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        '''
+        returns a string representation of Friend
+        '''
+        return f"{self.profile1} and {self.profile2}"
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -2,10 +2,10 @@
 # Author: John kim (johnjk@bu.edu), 10/07/2024
 # Description: this file sets the views for mini_fb
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from . models import *
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
 from .forms import CreateProfileForm, CreateStatusMessageForm, UpdateProfileForm, UpdateStatusMessageForm
 from django.urls import reverse
 
@@ -133,4 +133,54 @@ class UpdateStatusMessageView(UpdateView):
         '''
         return reverse('show_profile', kwargs={'pk': self.object.profile.pk})
 
+class CreateFriendView(View):
+    '''
+    view to add a friend
+    '''
+    def dispatch(self, request, *args, **kwargs):
+        '''
+        dispatch override
+        '''
+        profile = get_object_or_404(Profile, pk=kwargs['pk'])
+        friend = get_object_or_404(Profile, pk=kwargs['other_pk'])
+        profile.add_friend(friend)
+        return redirect('show_profile', pk=profile.pk)
     
+class ShowFriendSuggestionsView(DetailView):
+    '''
+    view to show friend suggestions
+    '''
+    model = Profile
+    template_name = 'mini_fb/friend_suggestions.html'
+    context_object_name = 'profile'
+
+    def get_context_data(self, **kwargs):
+        '''
+        addding friend suggestions to the context
+        '''
+        context = super().get_context_data(**kwargs)
+        
+        profile = self.get_object()
+        context['friend_suggestions'] = profile.get_friend_suggestions()
+        
+        return context
+    
+class ShowNewsFeedView(DetailView):
+    '''
+    view to show the news feed
+    '''
+    model = Profile
+    template_name = 'mini_fb/news_feed.html'
+    context_object_name = 'profile'
+
+    def get_context_data(self, **kwargs):
+        '''
+        adding the feed to the context
+        '''
+        context = super().get_context_data(**kwargs)
+
+        profile = self.get_object()
+
+        context['news_feed'] = profile.get_news_feed()
+
+        return context
